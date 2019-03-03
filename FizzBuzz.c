62af31c2af7f557b18c76917f627e200b602a8ae
otoyasi <syuuotsiro@icloud.com>
2019-03-03 21:06:28 +0900

    FizzBuzz.cを修正しました。

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
+include<stdio.h>
 int main(void){
     int a;
     int b;
     int c;
+
+    /*** 数値を入力する ***/
     scanf("%d",&a);
     scanf("%d",&b);
     
-    for(c=1;c<=30;c++){
-    if(c%a !=0 && c%b !=0){
-    printf("%d\n",c);
-    }else if(c%a  ==0 && c %b !=0){
-    printf("Fizz\n");
-    }else if( c % b ==0 && c %a !=0 ){
-    printf("Buzz\n");
-    }else if(c % a ==0 && c % b ==0){
-        printf("FizzBuzz\n");
-             }
-             }
+    for(int i=1; i<=30; i++){
+	c = i;
+    	if(c%a != 0 && c%b != 0){
+    		printf("%d\n",c);
+    	}else if(c%a == 0 && c%b != 0){
+    		printf("Fizz\n");
+    	}else if(c%b ==0 && c %a != 0){
+    		printf("Buzz\n");
+    	}else if(c % a ==0 && c % b == 0){
+        	printf("FizzBuzz\n");
+        }
+    }
     return 0;
 }
